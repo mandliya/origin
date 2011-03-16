e48983682c825eba252d3304eee041df1cbeb5ef
andrew.n.sutton@gmail.com <andrew.n.sutton@gmail.com@3dbf15f1-5c06-4b2d-4fb8-2ed049db4e7e>
2011-03-16 19:28:46 +0000

    Added a small test file to check for compilation success.

---
+++ sandbox/heaps/tests/compile.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2008-2010 Kent State University
+// Copyright (c) 2011 Texas A&M University
+//
+// This file is distributed under the MIT License. See the accompanying file
+// LICENSE.txt or http://www.opensource.org/licenses/mit-license.php for terms
+// and conditions.
+
+// The purpose of this file is to simply ensure that all header files in the
+// heaps library compile without warnings or errors.
+
+#include <origin/heaps/binomial_heap.hpp>
+
+int main()
+{
+}
