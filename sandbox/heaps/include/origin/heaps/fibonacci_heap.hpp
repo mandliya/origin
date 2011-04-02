a11c50478e728aafffcc489933c9ba66a7634969
saurabh.m.mnnit@gmail.com <saurabh.m.mnnit@gmail.com@3dbf15f1-5c06-4b2d-4fb8-2ed049db4e7e>
2011-04-02 02:33:00 +0000

    Placeholder for code to be added in future

---
+++ sandbox/heaps/include/origin/heaps/fibonacci_heap.hpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2008-2010 Kent State University
+// Copyright (c) 2011 Texas A&M University
+//
+// This file is distributed under the MIT License. See the accompanying file
+// LICENSE.txt or http://www.opensource.org/licenses/mit-license.php for terms
+// and conditions.
+
+#ifndef ORIGIN_HEAPS_FIBONACCI_HEAP_HPP
+#define ORIGIN_HEAPS_FIBONACCI_HEAP_HPP
+
+#include <vector>
+#include <iostream>
+#include <map>
+
+#endif // ORIGIN_HEAPS_FIBONACCI_HEAP_HPP
